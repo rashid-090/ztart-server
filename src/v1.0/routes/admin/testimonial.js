d465954a0fa2362777d8ea2f6c5d4568aaae737b
Rashid <rashidpk090@gmail.com>
2025-04-18 11:43:53 +0530

    alldone

--- src/v1.0/routes/admin/testimonial.js
+++ src/v1.0/routes/admin/testimonial.js
@@ -11,6 +11,8 @@ cloudinary.config({
   api_key: process.env.CLOUDINARY_API_KEY,
   api_secret: process.env.CLOUDINARY_API_SECRET,
 });
+
+
 const storage = new CloudinaryStorage({
   cloudinary: cloudinary,
   params: {
